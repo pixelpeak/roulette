afd0d5171894c5ff07392b55d41bfa437371cca8
fuchiwaki_rie <0896.riri@gmail.com>
2023-03-28 22:28:22 +0900

    clean: prettire

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-'use strict'; {
+"use strict";
+{
   class Panel {
     constructor() {
-      const music = new Audio('musics/MusMus-BGM-126.mp3');
-      const section = document.createElement('section');
-      section.classList.add('panel');
+      const music = new Audio("musics/MusMus-BGM-126.mp3");
+      const section = document.createElement("section");
+      section.classList.add("panel");
 
-      this.img = document.createElement('img');
+      this.img = document.createElement("img");
       this.img.src = this.getRandomImage();
 
       this.timeoutId = undefined;
-      this.spinStop = document.createElement('div');
-      this.spinStop.setAttribute('id', 'spin');
-      this.spinStop.textContent = 'スタート';
-      this.spinStop.addEventListener('click', () => {
-
-        if (this.spinStop.classList.contains('inactive')) {
+      this.spinStop = document.createElement("div");
+      this.spinStop.setAttribute("id", "spin");
+      this.spinStop.textContent = "スタート";
+      this.spinStop.addEventListener("click", () => {
+        if (this.spinStop.classList.contains("inactive")) {
           clearTimeout(this.timeoutId);
-          this.spinStop.classList.remove('inactive');
-          this.spinStop.classList.add('stop');
-          this.spinStop.textContent = 'スタート';
+          this.spinStop.classList.remove("inactive");
+          this.spinStop.classList.add("stop");
+          this.spinStop.textContent = "スタート";
           music.pause();
           return;
         }
-        this.spinStop.classList.remove('stop');
-        this.spinStop.classList.add('inactive');
+        this.spinStop.classList.remove("stop");
+        this.spinStop.classList.add("inactive");
         this.spin();
-        this.spinStop.textContent = 'ストップ';
+        this.spinStop.textContent = "ストップ";
         music.play();
         music.loop = true;
       });
@@ -33,17 +33,17 @@
       section.appendChild(this.img);
       section.appendChild(this.spinStop);
 
-      const content = document.getElementById('content');
+      const content = document.getElementById("content");
       content.appendChild(section);
     }
 
     getRandomImage() {
       const images = [
-        'img/gift-1.png',
-        'img/gift-2.png',
-        'img/gift-3.png',
-        'img/gift-4.png',
-        'img/gift-5.png'
+        "img/gift-1.png",
+        "img/gift-2.png",
+        "img/gift-3.png",
+        "img/gift-4.png",
+        "img/gift-5.png",
       ];
       return images[Math.floor(Math.random() * images.length)];
     }
@@ -57,7 +57,3 @@
   }
   new Panel();
 }
-
-
-	
-// $('#arc-text').arctext({radius: 300});
