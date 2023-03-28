7018069bdc6a320908b36a0470c659bd82cf9194
fuchiwaki_rie <0896.riri@gmail.com>
2023-03-28 23:21:40 +0900

    fix:all

--- js/main.js
+++ js/main.js
@@ -1,59 +1,28 @@
-"use strict";
-{
-  class Panel {
-    constructor() {
-      const music = new Audio("musics/MusMus-BGM-126.mp3");
-      const section = document.createElement("section");
-      section.classList.add("panel");
+var frames = document
+  .getElementsByClassName("anime")[0]
+  .getElementsByTagName("img");
+viewFrame();
 
-      this.img = document.createElement("img");
-      this.img.src = this.getRandomImage();
-
-      this.timeoutId = undefined;
-      this.spinStop = document.createElement("div");
-      this.spinStop.setAttribute("id", "spin");
-      this.spinStop.textContent = "スタート";
-      this.spinStop.addEventListener("click", () => {
-        if (this.spinStop.classList.contains("inactive")) {
-          clearTimeout(this.timeoutId);
-          this.spinStop.classList.remove("inactive");
-          this.spinStop.classList.add("stop");
-          this.spinStop.textContent = "スタート";
-          music.pause();
-          return;
-        }
-        this.spinStop.classList.remove("stop");
-        this.spinStop.classList.add("inactive");
-        this.spin();
-        this.spinStop.textContent = "ストップ";
-        music.play();
-        music.loop = true;
-      });
-
-      section.appendChild(this.img);
-      section.appendChild(this.spinStop);
-
-      const content = document.getElementById("content");
-      content.appendChild(section);
-    }
-
-    getRandomImage() {
-      const images = [
-        "img/gift-1.png",
-        "img/gift-2.png",
-        "img/gift-3.png",
-        "img/gift-4.png",
-        "img/gift-5.png",
-      ];
-      return images[Math.floor(Math.random() * images.length)];
-    }
-
-    spin() {
-      this.img.src = this.getRandomImage();
-      this.timeoutId = setTimeout(() => {
-        this.spin();
-      }, 100);
-    }
+function viewFrame(frame_no = -1) {
+  if (frames[frame_no]) {
+    frames[frame_no].style.display = "none";
   }
-  new Panel();
+  frame_no++;
+  if (frames[frame_no]) {
+    frames[frame_no].style.display = "block";
+  } else {
+    frames[0].style.display = "block";
+    frame_no = 0;
+  }
+  var msec = document.getElementById("anime_speed").value;
+  setTimeout(function () {
+    viewFrame(frame_no);
+  }, msec);
+}
+
+const music = new Audio("musics/MusMus-BGM-126.mp3");
+
+function play() {
+  music.loop = true;
+  music.play();
 }
